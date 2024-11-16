ef0038cbf2a6bb422b87951601a88a8ff88ef513
Anantha Krishna B <157831286+akbted@users.noreply.github.com>
2024-11-16 16:17:03 +0530

    Improve index view to handle non-existent Todolists and display all Todoitems

--- database_basics/tododatabase/views.py
+++ database_basics/tododatabase/views.py
@@ -1,3 +1,6 @@
+# Keep in mind the id = 3 because we have only one list in the database and its id is 3. 1 and 2 I deleted them.
+
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todolist, Todoitem
@@ -5,10 +8,15 @@ from .models import Todolist, Todoitem
 # Create your views here.
 
 
-def index(reuqest, id):
-    ls = Todolist.objects.get(id=id)
-    item = ls.todoitem_set.get(id=1)
-    return HttpResponse(f"<h1>{ls.name}</h1>")
+def index(request, id):
+    try:
+        ls = Todolist.objects.get(id=id)
+    except Todolist.DoesNotExist:
+        return HttpResponse(f"<h1>Todolist with id {id} does not exist.</h1>")
 
+    items = ls.todoitem_set.all()
+    if not items:
+        return HttpResponse(f"<h1>No Todoitems in Todolist {ls.name}.</h1>")
 
-# Keep in mind the id = 3 because we have only one list in the database and its id is 3. 1 and 2 I deleted them.
+    items_text = ", ".join([item.text for item in items])
+    return HttpResponse(f"<h1>{ls.name}: {items_text}</h1>")
