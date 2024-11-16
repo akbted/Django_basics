aba24b2a62e53a1cbb85f76358335a0c6343da21
Anantha Krishna B <157831286+akbted@users.noreply.github.com>
2024-11-16 16:09:47 +0530

    Update URL pattern and modify index view to accept list ID

--- database_basics/tododatabase/urls.py
+++ database_basics/tododatabase/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("page1", views.index, name="index")]
+urlpatterns = [path("<int:id>", views.index, name="index")]
--- database_basics/tododatabase/views.py
+++ database_basics/tododatabase/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todolist, Todoitem
 
 # Create your views here.
 
 
-def index(reuqest):
-    return HttpResponse("Hello, world. You're at the tododatabase index.")
+def index(reuqest, id):
+    ls = Todolist.objects.get(id=id)
+    item = ls.todoitem_set.get(id=1)
+    return HttpResponse(f"<h1>{ls.name}</h1>")
+
+
+# Keep in mind the id = 3 because we have only one list in the database and its id is 3. 1 and 2 I deleted them.
